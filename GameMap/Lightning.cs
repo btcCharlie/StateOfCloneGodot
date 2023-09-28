13401af7ebceeb89d695ff1e8ed40ec98d2b72b4
btCharlie <thomas.b.charlie@gmail.com>
2023-09-28 10:28:32 +0200

    coroutine lightning
    added an async-await asynchronous call to the lightning light so it gets turned off after an amount of time

--- GameMap/Lightning.cs
+++ GameMap/Lightning.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+using System.Threading.Tasks;
 
 public partial class Lightning : OmniLight3D
 {
@@ -11,6 +12,7 @@ public partial class Lightning : OmniLight3D
     public override void _Ready()
     {
         lastTick = Time.GetTicksMsec();
+        Visible = false;
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -18,7 +20,17 @@ public partial class Lightning : OmniLight3D
     {
         // float timeDiff = Time.GetTicksMsec() - lastTick;
 
+        if (GD.Randf() > threshold)
+        {
+            EnableAndDisableNode(0.2f);
+        }
 
+    }
 
+    private async void EnableAndDisableNode(float durationInSeconds)
+    {
+        Visible = true;
+        await Task.Delay(TimeSpan.FromSeconds(durationInSeconds));
+        Visible = false;
     }
 }
